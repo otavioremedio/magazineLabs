230523a3ad750d013c2ecf2107213f805c71d396
otavioremedio <otavioremedio@gmail.com>
2018-02-14 19:32:57 -0200

    datasql

--- src/main/resources/data.sql
+++
@@ -1,5 +0,0 @@
-INSERT INTO EMPLOYEE (NAME, EMAIL, DEPARTMENT) VALUES ('Rodrigo Carvalho', 'rodrigo@luizalabs.com', 'IntegraCommerce');
-INSERT INTO EMPLOYEE (NAME, EMAIL, DEPARTMENT) VALUES ('Renato Pedigoni', 'rodrigo@luizalabs.com', 'Digital Platform');
-INSERT INTO EMPLOYEE (NAME, EMAIL, DEPARTMENT) VALUES ('Thiago Catoto', 'catoto@luizalabs.com', 'Mobile');
-
-
